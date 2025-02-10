115c7b6b63b289142bb007525ef18d3b6ad17373
Carl <blomecj@rose-hulman.edu>
2025-02-10 14:52:04 -0500

    added my TODOs to xv6test.c

--- xv6-riscv/user/xv6test.c
+++ xv6-riscv/user/xv6test.c
@@ -1,6 +1,10 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+
+//TODO: create thread methods (one that accepts args two that just print whatever)
+
+
 int main(int argc, char *argv[]) {
   uint64 p = 0xdeadbeef;
 
@@ -8,6 +12,8 @@ int main(int argc, char *argv[]) {
   thread_create(0, 0, 0);
   thread_join(0);
   thread_exit(0);
+//TODO: Write code that creates the threads and gives them specified functions and arguments
+//sidenote if I am adding args how will that work without having shared stacks yet? (maybe using a global will work because of copied stuff stack might also work because of copied page table)
 
   exit(0);
 }
