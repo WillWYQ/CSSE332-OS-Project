8e736645a5acf6118b1b11f7cb7d330085de7e9c
Carl <blomecj@rose-hulman.edu>
2025-02-11 01:38:10 -0500

    added tests for thread_create still need to add ability to select which test to run

--- xv6-riscv/user/xv6test.c
+++ xv6-riscv/user/xv6test.c
@@ -2,18 +2,60 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+#define NUM_THREADS 3
 //TODO: create thread methods (one that accepts args two that just print whatever)
+void test_thread_create_fn1(void * args){
+  printf("This thread is running test_thread_create_fn1 at %p\n", test_thread_create_fn1);
+  return;
+}
+
+void test_thread_create_fn2(void * args){
+  printf("This thread is running test_thread_create_fn2 at %p\n", test_thread_create_fn2);
+  return;
+}
+
+void test_thread_create_fn3(void * args){
+  int tid = *(int *) args;
+  printf("This is thread %d running test_thread_create_fn3 at %p\n", tid, test_thread_create_fn3);
+  return;
+}
 
 
-int main(int argc, char *argv[]) {
+void test_thread_start_at_pc_m2(void){
+  //TODO: talk with team about how the final tid arg works
+  //TODO: add join lower in this
+  printf("===============Threads Starting at Arbitrary Function No Args==============\n");
+  thread_create(0, test_thread_create_fn1, 0);//no Args
+  thread_create(0, test_thread_create_fn2, 0);//no Args
+}
+
+void test_thread_with_args_m2(void){
+  //TODO: talk with team about how the final tid arg works
+  //TODO: add join lower in this
+  printf("===============Threads With Args==============\n");
+  int tids[NUM_THREADS];
+  for(int i = 0; i < NUM_THREADS; i++){
+    tids[i] = i;
+    thread_create(&tids[i], test_thread_create_fn3, 0);//with Args
+  }
+}
+
+void test_spoon_m1(void){
+  printf("===============Spoon & M1==============\n");
   uint64 p = 0xdeadbeef;
-
   spoon((void*)p);
   thread_create(0, 0, 0);
   thread_join(0);
-  thread_exit(0);
-//TODO: Write code that creates the threads and gives them specified functions and arguments
-//sidenote if I am adding args how will that work without having shared stacks yet? (maybe using a global will work because of copied stuff stack might also work because of copied page table)
+  //thread_exit(0);
+}
+
+int main(int argc, char *argv[]) {
+  //TODO: Write code to select what tests to run
+  test_thread_start_at_pc_m2();
+
+  test_thread_with_args_m2();
+
+  test_spoon_m1();
 
   exit(0);
 }
